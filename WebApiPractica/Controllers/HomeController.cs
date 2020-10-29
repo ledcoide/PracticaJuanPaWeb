0e791cdb09aa8dc814c3338383e65ec3ae636a38
LEDCO-PC\LEDCO <luis0ortiz@hotmail.com>
2020-10-28 20:17:23 -0400

    first commit

---
+++ WebApiPractica/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using APIFundapecUnapec.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiPractica.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://10.0.0.2:45456/api/Estudiantes");
+            var estudiantesList = JsonConvert.DeserializeObject<List<Estudiante>>(json);
+            return View(estudiantesList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
